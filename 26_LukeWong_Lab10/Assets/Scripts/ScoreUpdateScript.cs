9ef8cd5d8c5ee70cdd5c67718315e63a43905122
27-LukeWong8888888888888888888888888888 <155930104+27-LukeWong8888888888888888888888888888@users.noreply.github.com>
2024-01-23 14:53:14 +0800

    Uploaded Unity File

---
+++ 26_LukeWong_Lab10/Assets/Scripts/ScoreUpdateScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUpdateScript : MonoBehaviour
+{
+    public Text ScoreText;
+    int score;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            score += 10;
+            ScoreText.text = "Score: " + score;
+        }
+    }
+}
---
+++ 26_LukeWong_Lab10/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject SpawnObject1;
+    public GameObject SpawnObject2;
+    public GameObject SpawnObject3;
+    float PositionY;
+    int RandomNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+
+    }
+
+    void SpawnObjects()
+    {
+        PositionY = Random.Range(4, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        RandomNumber = Random.Range(1, 4);
+        if (RandomNumber == 1)
+        {
+            Instantiate(SpawnObject1, transform.position, transform.rotation);
+        }
+        else if (RandomNumber == 2)
+        {
+            Instantiate(SpawnObject2, transform.position, transform.rotation);
+        }
+        else if (RandomNumber == 3)
+        {
+            Instantiate(SpawnObject3, transform.position, transform.rotation);
+        }
+    }
+}
